f4c004602dc629128aebf08b8eb97c6858f98489
Anup Malleswarapu <132218710+mvsanup17@users.noreply.github.com>
2025-01-08 17:23:35 +0530

    Update yannick.jsx

--- client/frontend/src/components/yannick.jsx
+++ client/frontend/src/components/yannick.jsx
@@ -17,7 +17,7 @@ function Yannick() {
                 <div className="row">
                     <div className="col-lg-4">
                         {/* <img src={imgs} alt="yannick" className="img-fluid" /> */}
-                        <img src={`http://localhost:5000/details/file_1705230792704.jpeg`} alt="yannick" className="img-fluid" />
+                        <img src={`https://food-mern-project-cxzb.vercel.app/details/file_1705230792704.jpeg`} alt="yannick" className="img-fluid" />
                     </div>
                     <div className="col-lg-8">
                         <h2>Yannick Alleno</h2>
