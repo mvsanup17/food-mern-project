937316c45f166a9b0d3c760a456071a9e533c9b9
Anup Malleswarapu <132218710+mvsanup17@users.noreply.github.com>
2025-01-09 17:31:53 +0530

    Update files.jsx

--- client/frontend/src/components/files.jsx
+++ client/frontend/src/components/files.jsx
@@ -12,14 +12,14 @@ function UploadItems() {
     const uploadFiles = (e) =>{
         const formdata = new FormData()
         formdata.append('file',file)
-        axios.post("https://food-mern-project-cxzb.vercel.app/uploaddata",formdata)
+        axios.post("https://food-mern-project-server.vercel.app/uploaddata",formdata)
         .then(res => alert(res.data.msg))
         window.location.reload()
         .catch(err => console.log(err))
     }
 
     useEffect(() => {
-        axios.get("https://food-mern-project-cxzb.vercel.app/getimage")
+        axios.get("https://food-mern-project-server.vercel.app/getimage")
         .then(res => setImages(res.data[0].images))
         .catch(err => console.log(err))
     },[])
